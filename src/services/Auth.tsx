de2d5917d1ff121c26c68e3a1ee8880842530b7b
Everton Leonel <everton_leonel@hotmail.com>
2022-12-13 22:29:41 -0300

    add services api and userAuth

---
+++ src/services/Auth.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { Ilogin } from '../pages/login/LoginForm';
+import { GET_USER } from './Api';
+
+export const userAuth = async (values: Ilogin) => {
+  try {
+    const response = await axios.get(GET_USER);
+
+    if (response && response.status === 200) {
+      const logins = response.data;
+
+      const found = logins.find((login: Ilogin) => {
+        return (
+          login.email === values.email && login.password === values.password
+        );
+      });
+      if (found) {
+        return found;
+      } else {
+        return false;
+      }
+    }
+  } catch (error) {
+    Promise.reject(error);
+  }
+};
