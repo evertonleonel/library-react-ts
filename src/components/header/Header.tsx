cc0501ead698ed7abfdce02627fc5d73e2e94279
Everton Leonel <everton_leonel@hotmail.com>
2022-12-16 16:05:49 -0300

    add navigation in logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,16 @@
 import Logo from '../../assets/logo.svg';
 import IconeUsuario from '../../assets/icone_usuario.svg';
 import Seta from '../../Assets/seta.svg';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { HeaderContainer, HeaderLogo, HeaderUsuario } from './HeaderStyles';
 import { Menu, MenuItem } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import UserContext from '../../context/UserContext';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
+  const { user, userLogout } = React.useContext(UserContext);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-
   const open = Boolean(anchorEl);
 
   const handleClick = (event: React.MouseEvent<HTMLImageElement>) => {
@@ -19,26 +20,43 @@ const Header = () => {
     setAnchorEl(null);
   };
 
-  return (
-    <HeaderContainer>
-      <HeaderLogo>
-        <img src={Logo} />
-      </HeaderLogo>
+  const handleLogout = () => {
+    setAnchorEl(null);
+    userLogout();
+  };
 
-      <HeaderUsuario>
-        <img src={IconeUsuario} alt="Icone Usuário: abrir opções" />
-        <p>Usuário</p>
-        <img src={Seta} alt="Icone seta: abrir opções" onClick={handleClick} />
-        <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-          <MenuItem
-            onClick={handleClose}
-            style={{ width: '110px', marginTop: '5px', fontSize: '18px' }}
-          >
-            Sair
-          </MenuItem>
-        </Menu>
-      </HeaderUsuario>
-    </HeaderContainer>
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderLogo
+          onClick={() => {
+            navigate('/home');
+          }}
+        >
+          <img src={Logo} />
+        </HeaderLogo>
+
+        <HeaderUsuario>
+          <img src={IconeUsuario} alt="Icone Usuário: abrir opções" />
+          {user ? <p>{user}</p> : <p>Usuário</p>}
+
+          <img
+            src={Seta}
+            alt="Icone seta: abrir opções"
+            onClick={handleClick}
+          />
+          <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+            <MenuItem
+              onClick={handleLogout}
+              style={{ width: '110px', marginTop: '5px', fontSize: '18px' }}
+            >
+              Sair
+            </MenuItem>
+          </Menu>
+        </HeaderUsuario>
+      </HeaderContainer>
+      <Outlet />
+    </>
   );
 };
 
