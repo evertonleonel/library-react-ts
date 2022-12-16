aa5090dbb50543c8cd42212f721b21f1836e5218
Everton Leonel <everton_leonel@hotmail.com>
2022-12-16 14:32:25 -0300

    fix validation moved to login folder

--- src/components/context/validate.tsx
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export const validateSchema = yup.object({
-  email: yup
-    .string()
-    .email('E-mail inválido')
-    .required('O campo E-mail é obrigatório'),
-  password: yup.string().required('O campo de senha é obrigatório'),
-});
