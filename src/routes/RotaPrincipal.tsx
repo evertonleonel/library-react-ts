7145c981a9c2a3d64c97f177aa893ccef635a059
Everton Leonel <everton_leonel@hotmail.com>
2022-12-20 16:53:37 -0300

    add new routes

--- src/routes/RotaPrincipal.tsx
+++ src/routes/RotaPrincipal.tsx
@@ -1,13 +1,31 @@
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { UserStorage } from '../context/UserContext';
 import Login from '../pages/login/Login';
+import Header from '../components/header/Header';
+import Home from '../pages/home/Home';
+import Cadastro from '../pages/cadastro/Cadastro';
+import Biblioteca from '../pages/biblioteca/Biblioteca';
+import Emprestimo from '../pages/emprestimo/Emprestimo';
 
 function RotaPrincipal() {
+  const userLogged = !!localStorage.getItem('user');
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
+      <UserStorage>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {userLogged && (
+            <Route path="/" element={<Header />}>
+              <Route path="home" element={<Home />} />
+              <Route path="cadastro" element={<Cadastro />} />
+              <Route path="biblioteca" element={<Biblioteca />} />
+              <Route path="emprestimo" element={<Emprestimo />} />
+            </Route>
+          )}
+        </Routes>
+      </UserStorage>
     </BrowserRouter>
   );
 }
