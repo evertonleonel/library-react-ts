8895669fbf7e1e68562342308390d4dd5a859812
Everton Leonel <everton_leonel@hotmail.com>
2022-12-22 19:11:19 -0300

    fix separating components

--- src/pages/biblioteca/Biblioteca.tsx
+++ src/pages/biblioteca/Biblioteca.tsx
@@ -4,13 +4,80 @@ import LinkBackHome from '../../components/linkBackHome/LinkBackHome';
 import { BibliotecaContainer } from './BibliotecaStyles';
 import BibliotecaBuscar from './BibliotecaBuscar';
 import BibliotecaLivros from './BibliotecaLivros';
+import { getBooks } from '../../services/GetBooks';
+import { IBook } from '../../interfaces/book';
+import Modal from '@mui/material/Modal';
+import { Box } from '@mui/material';
+import ModalBook from '../../components/modals/modalBook/ModalBook';
 
 const Biblioteca: React.FC = () => {
+  const [open, setOpen] = React.useState(false);
+  const OpenModal = () => setOpen(true);
+  const CloseModal = () => setOpen(false);
+
+  const [books, setBooks] = React.useState<IBook[]>();
+  const [loadBooks, setLoadBooks] = React.useState<IBook[]>();
+  const [inputPesquisar, setInputPesquisar] = React.useState('');
+  const [selectFiltro, setSelectFiltro] = React.useState('');
+  const [livroId, setLivroId] = React.useState('');
+
+  React.useEffect(() => {
+    getBooks().then((data) => setBooks(data));
+  }, []);
+
+  function getIDLivro(e: React.MouseEvent<HTMLLIElement>) {
+    if (typeof e.currentTarget.id === 'string') {
+      setLivroId(e.currentTarget.id);
+      OpenModal();
+    }
+  }
+
+  function inputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setInputPesquisar(value);
+  }
+
+  function filtroChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.target;
+    setSelectFiltro(value);
+  }
+
+  const filtrarLivros = async (pesquisa: string, tipo: string) => {
+    if (books) {
+      console.log(books);
+      const FilterBooks = books.filter(
+        (book) => book[tipo].toLowerCase().indexOf(pesquisa.toLowerCase()) > -1
+      );
+      setLoadBooks(FilterBooks);
+    }
+  };
+
+  function buscarLivros() {
+    filtrarLivros(inputPesquisar, selectFiltro);
+  }
+
   return (
     <BibliotecaContainer>
       <LinkBackHome page="Biblioteca" />
-      <BibliotecaBuscar />
-      <BibliotecaLivros />
+      <BibliotecaBuscar
+        inputChange={inputChange}
+        buscarLivros={buscarLivros}
+        filtroChange={filtroChange}
+        selectFiltro={selectFiltro}
+      />
+
+      <Modal open={open} onClose={CloseModal}>
+        <>
+          <ModalBook livroId={livroId} />
+        </>
+      </Modal>
+
+      {books && (
+        <BibliotecaLivros
+          books={loadBooks ? loadBooks : books}
+          getIDLivro={getIDLivro}
+        />
+      )}
     </BibliotecaContainer>
   );
 };
--- src/pages/biblioteca/BibliotecaBuscar.tsx
+++ src/pages/biblioteca/BibliotecaBuscar.tsx
@@ -4,20 +4,26 @@ import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
 import { PesquisaContainer } from './BibliotecaStyles';
 
-// interface ISearch {
-//   value: string;
-//   onChange: () => void;
-//   label: string;
-//   helperText: string;
-// }
+interface ISearch {
+  selectFiltro: string;
+  inputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  buscarLivros: () => void;
+  filtroChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-const BibliotecaBuscar = () => {
+const BibliotecaBuscar = ({
+  inputChange,
+  buscarLivros,
+  filtroChange,
+  selectFiltro,
+}: ISearch) => {
   return (
     <PesquisaContainer>
       <div className="buscar">
         <SearchIcon sx={{ fontSize: 20, color: '#ADB5BD' }} />
-        <input placeholder="Pesquisar livro..." />
+        <input onChange={inputChange} placeholder="Pesquisar livro..." />
         <Button
+          onClick={buscarLivros}
           sx={{
             backgroundColor: '#FFC501',
             color: '#000000',
@@ -32,11 +38,19 @@ const BibliotecaBuscar = () => {
       </div>
 
       <div className="filtrar">
-        <TextField id="filtro" name="filtro" label="Filtrar" select fullWidth>
-          <MenuItem value="Gênero">Gênero</MenuItem>
-          <MenuItem value="Autor">Autor</MenuItem>
-          <MenuItem value="Data de Entrada">Data de Entrada</MenuItem>
-          <MenuItem value="Sinopse">Sinopse</MenuItem>
+        <TextField
+          id="filtro"
+          name="filtro"
+          label="Filtrar"
+          value={selectFiltro}
+          onChange={filtroChange}
+          select
+          fullWidth
+        >
+          <MenuItem value="genre">Gênero</MenuItem>
+          <MenuItem value="author">Autor</MenuItem>
+          <MenuItem value="systemEntryDate">Data de Entrada</MenuItem>
+          <MenuItem value="tittle">Título</MenuItem>
         </TextField>
       </div>
     </PesquisaContainer>
